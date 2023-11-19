6160561b719d58e5c325aa8acee64b5c514feb6c
Stephen Camilon <113228328+Stephencamilon@users.noreply.github.com>
2023-11-19 16:19:24 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -9,38 +9,32 @@ contract Assessment {
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event Burn(uint256 amount);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns(uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
 
         // perform transaction
         balance += _amount;
 
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
         // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
+
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
@@ -51,10 +45,19 @@ contract Assessment {
         // withdraw the given amount
         balance -= _withdrawAmount;
 
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
         // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function burn(uint256 _burnAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(_burnAmount <= balance, "Burn amount exceeds the available balance");
+
+        if (_burnAmount < 5) {
+            revert("BURN MOREEEEEEEEEEEE");
+        }
+
+        balance -= _burnAmount;
+        emit Burn(_burnAmount);
     }
 }
